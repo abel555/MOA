d5bcb1429f94c900eb5417faa188ad541086c036
abel555 <taqui555@gmail.com>
2019-06-02 00:11:28 -0400

    buscar por nombre en las tablas

--- src/JS/showListWoods.js
+++ src/JS/showListWoods.js
@@ -2,6 +2,23 @@ const path = require('path');
 const fs = require('fs');
 const jsonFilename = path.resolve(__dirname, '..', 'data', 'WOODS_DETAILS.json');
 
+function search() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("woodList");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
 function getWoodListFromJsonFile() {
   let woodsJSON = fs.readFileSync(jsonFilename);
   let woodList = JSON.parse(woodsJSON);
