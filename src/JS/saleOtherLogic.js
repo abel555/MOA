a439695fc9b7d3c0261a9421b4e2b4b3ae6f5182
abel555 <taqui555@gmail.com>
2019-06-18 04:18:36 -0400

    mis cambios

--- src/JS/saleOtherLogic.js
+++ src/JS/saleOtherLogic.js
@@ -2,11 +2,19 @@ const ipc = require('electron').ipcRenderer;
 const { ipcRenderer } = require('electron');
 const CurrentProductController = require("../JS/CurrentProductController")
 const currentProductController = new CurrentProductController();
+const ShoppingCartController = require('../JS/ShoppingCartController');
+const shoppingCartController = new ShoppingCartController();
 const currentProduct = getCurrent();
 const punit = document.getElementById("punit");
 let product;
 async function getCurrent(){
     return await currentProductController.getCurrentProduct();
+}
+async function sendProduct(product) {
+    await shoppingCartController.addNewProductToShoppingCart(product);
+}
+async function getAllCart() {
+    return await shoppingCartController.getAllProducts();
 }
 const form = document.querySelector('form');
 ipc.on('message', function(event, message){
@@ -23,10 +31,11 @@ form.addEventListener('submit', async event => {
 
     cant = parseFloat(cant)
     product.quantity = cant;
-    product.total_sold = cant * parseFloat(punit.value);
-    product.type = currentProduct;
-    console.log(product);
-    //ipcRenderer.send('product:new', product);
+    product.total_cost = cant * parseFloat(punit.value);
+    product.typeProduct = await getCurrent();
+    await shoppingCartController.addNewProductToShoppingCart(product);
+    // console.log(await shoppingCartController.getAllProducts());
+     ipcRenderer.send('product:new', product);
    
 
 });
