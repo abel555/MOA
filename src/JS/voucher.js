39ff974ff99fd7289f59b45eb47f6254e8a4a60a
abel555 <taqui555@gmail.com>
2019-06-20 11:19:08 -0400

    merge de master

--- src/JS/voucher.js
+++ src/JS/voucher.js
@@ -20,7 +20,34 @@ menu.append(new MenuItem({
         ipcRenderer.send('print-to-pdf', product);
     }
 }));
-
+function chargeListInTable(woodsList){
+    const tableBody = document.getElementById("table-body");
+    for(i = 0; i < woodsList.length; i++) {
+      
+        if(woodsList[i].counter)
+            continue;
+        let content = `
+        <tr class="dbl-click">
+            <td class="clickable" onclick="showModal(${i})">${woodsList[i].idProduct}</td>
+            <td id="dbl-menu" class="dbl-click">${woodsList[i].name_product}</td>
+            <div id="${i}" class="dropdown-content">
+                <a href="#">Editar</a>
+                <a href="#" onclick="renderSaleW(${i})">Agregar a venta</a>
+            </div>
+            <td>${woodsList[i].provider}</td>
+            <td>${woodsList[i].descriptionProduct}</td>
+            <td>${woodsList[i].quantity}</td>
+            <td>${woodsList[i].quantity_sold}</td>
+            <td>${woodsList[i].purchase_price}</td>
+            <td>${woodsList[i].sale_price}</td>
+            <td>${woodsList[i].purchased_total}</td>
+            <td>${woodsList[i].total_sold}</td>
+            <td>${woodsList[i].reaminingAmount}</td>
+        </tr>
+        `;
+        tableBody.insertAdjacentHTML('beforeEnd',content);
+    }
+}
 const tp = document.getElementById("to-print");
 tp.addEventListener('click', event => {
     ipcRenderer.send('print-to-pdf', product);
